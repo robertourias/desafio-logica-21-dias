ed6f90bc7d78e82477f3ede46327bfd9b6560d71
Roberto Nicoletti <roberto.urias@gmail.com>
2025-04-04 14:12:15 -0300

    iniciando desafio do canal codigo fonte

---
+++ dia-1.ts
@@ -0,0 +1,72 @@
+// Desafio 1: Algoritmo para acordar
+
+/**
+1 - Desligar o despertador
+2 - Sair da cama
+3 - Ir até o banheiro
+4 - Usar o banheiro
+5 - Escovar os dentes
+6 - Ir até a cozinha
+7 - Tomar um copo de água
+8 - Procurar meus gatos
+9 - Cheirar e apertar gatos
+10 - Abrir as janelas
+11 - Ir até o quarto
+12 - Me trocar
+13 - Ir dar uma caminhada no condominio
+14 - Voltar para o apartamento
+15 - Ir até a cozinha
+16 - Preparar um café da manhã
+17 - Levar meu café da manhã até a sala
+18 - Tomar meu café
+*/
+
+// Desafio 2: Escreva sua receita favorita
+/**
+1 - Pegar meia xícara de cuzcuz
+2 - Colocar em uma vazilha com meia xícara de água
+3 - Adicionar meia colher de café de sal
+4 - Misturar
+5 - Deixar ela reservada por 10 minutos
+6 - Colocar 100ml de água na cuzcuzeira
+7 - Colocar o cuzcuz úmido na cuzcuzeira
+8 - Coloca-la no fogão em fogo médio
+9 - Aguardar 1 min
+10 - Diminuir fogo
+11 - Aguardar 10 minutinhos cozinhando
+12 - Servir o cuzcuz em um prato ou recipiente de preferencia
+13 - Colocar meia colher bem raza de manteiga em cima
+14 - Esquentar 80gr de frango desfiado por 1 min no microondas
+15 - Adicionar ao recipiente com cuzcuz
+16 - Adicionar 1 colher de sopa raza de semente de girassol e semente de abóbora
+17 - Misturar
+18 - Adicionar uma salada feita previamente de alface, rucula e tomate
+19 - Preparar o café expresso com pouco açucar
+20 - Se deliciar se sentindo abençoado kkkk
+*/
+
+// Desafio 3: Monte sua lista de tarefas diéarias
+
+/**
+1 - Levantar
+2 - Tomar água
+3 - Caminhar
+4 - Tomar um café da manhã
+5 - Tarefas de rotina da manhã
+6 - Organizar agenda
+7 - Planejar tarefas do dia
+8 - Trabalhar
+9 - Ir para academia treinar
+10 - Almoçar
+11 - Continuar trabalho 
+12 - Meditar
+13 - Tomar um café da tarde
+14 - Estudar inglês
+15 - Alongamento
+16 - Aerobio do dia
+17 - Tomar um banho
+18 - Jantar
+19 - Relaxar fazendo algo que gosto
+20 - Colocar as coisas no lugar em casa
+21 - Ir dormir
+*/
